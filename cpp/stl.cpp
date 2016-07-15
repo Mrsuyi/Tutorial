3c08b6c486bb05e59fe6a7446d48df97e128bc36
Mrsuyi <mrsuyi@163.com>
2016-07-15 19:23:53 +0800

    stl

---
+++ cpp/stl.cpp
@@ -0,0 +1,45 @@
+// constainer
+#include <vector>
+#include <array>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+// io
+#include <iostream>
+#include <fstream>
+
+// thread
+#include <mutex>
+#include <thread>
+
+// else
+#include <algorithm>
+#include <string>
+#include <memory>
+#include <regex>
+#include <typeinfo>
+
+using namespace std;
+
+template <typename K, typename V>
+using umap = unordered_map<K, V>;
+template <typename T>
+using uset = unordered_set<T>;
+
+template <typename T>
+using sptr = shared_ptr<T>;
+template <typename T>
+using uptr = unique_ptr<T>;
+template <typename T>
+using wptr = weak_ptr<T>;
+
+
+int main(int argc, char* argv[])
+{
+    return 0;
+} 
