7e835dc977be8c281550dae572e9209293e3d348
Mrsuyi <mrsuyi@163.com>
2016-11-30 19:34:51 +0800

    python & js

--- c/struct.c
+++ c/struct.c
@@ -1,32 +1,56 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-struct shit
+typedef struct
 {
-};
+    int num;
+    char tail[];
+} shit;
+
+void
+array_tail()
+{
+    assert(sizeof(shit) == 4);
+
+    shit* s = (shit*)malloc(sizeof(shit) + 5);
+
+    s->tail[0] = 's';
+    s->tail[1] = 'h';
+    s->tail[2] = 'i';
+    s->tail[3] = 't';
+    s->tail[4] = 0;
+
+    printf("%s\n", s->tail);
+
+    free(s);
+}
 
 typedef struct fuck
 {
-    union
-    {
-        int    numi;
+    union {
+        int numi;
         double numd;
     };
     const char* name;
-}
-fuck;
+} fuck;
 
-int main()
+void
+brace_init()
 {
-    fuck fucks[] = {{123, "1231"}, {3245, "2345"}, {0, 0}};
+    fuck fucks[] = {{{123}, "1231"}, {{3245}, "2345"}, {{0}, 0}};
 
     for (int i = 0; fucks[i].numi != 0; ++i)
-    { 
+    {
         printf("%d  %s\n", fucks[i].numi, fucks[i].name);
     }
+}
 
-    int a, b;
-
-    a = b = 1;
+int
+main()
+{
+    array_tail();
+    brace_init();
 
     return 0;
 }
---
+++ python/http/simple_http_server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+print("serving at port", PORT)
+
+httpd.serve_forever()
